16325f7ffc6e7a84effe5946567cccb1d1ee4bc3
15609075281 <676122503@qq.com>
2018-07-12 18:04:20 +0800

    1
    1

--- apps/gain_phone/staticMethods/save_csv.py
+++ apps/gain_phone/staticMethods/save_csv.py
@@ -5,8 +5,23 @@ import datetime
 import time
 
 
-def save_csv():
+# 存储到本地csv文件夹里
+def save_csv(path, arr_, arr_data):
     print('存储到本地csv文件夹里')
+    # 判断文件是否存在
+    if os.access(path, os.F_OK):
+        with open(path, 'a+', newline='', encoding='utf-8-sign') as save_c:
+            wr_csv = csv.writer(save_c)
+            wr_csv.writerows(arr_data)
+            print('存在+存储成功')
+            save_c.close()
+    else:
+        with open(path, 'w', newline='', encoding='utf-8-sign') as save_c:
+            wr_csv = csv.writer(save_c)
+            wr_csv.writerow(arr_)  # 存储数据样式['','']
+            wr_csv.writerows(arr_data)  # 存储的数据[{},{}]
+            print('不存在+创建存储成功')
+            save_c.close()
 
 
 if __name__ == '__main__':
